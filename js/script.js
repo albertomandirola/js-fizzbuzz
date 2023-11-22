eaab6d307612aa2515a6eb05bb25d62a16f2eb26
albertomandirola <albymandi00@gmail.com>
2023-11-22 16:02:04 +0100

    added li item by script.js to index.html

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
-
+const lista = document.querySelector("ul");
+let li;
 for (i=1 ; i<=100 ; i++ ){
     if(i%3 == 0 && i%5 == 0){
+        li = `<li>FizzBuzz</li>`
         console.log("FizzBuzz")
     }
     else if(i%3 == 0){
+        li = `<li>Fizz</li>`
         console.log("Fizz")
     }
     else if(i%5 == 0){
+        li = `<li>Buzz</li>`
         console.log("Buzz")
     }
     else{
+        li = `<li>${i}</li>`
         console.log(i)
     }
+    lista.innerHTML += li
 }
